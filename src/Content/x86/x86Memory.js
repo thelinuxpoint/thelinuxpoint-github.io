1cf9b55fc795b18e4d9cd1b28cad4a36f5ce1faf
thelinuxpoint <choudharyprakash066@gmail.com>
2022-12-04 00:06:36 +0530

    updates

--- src/Content/x86/x86Memory.js
+++ src/Content/x86/x86Memory.js
@@ -37,9 +37,14 @@ export default function X86Memory(){
 			Logical addresses are often referred to as far pointers. Far pointers are used in software addressing
 			when the segment reference must be explicit (i.e., a reference to a segment outside the current
 			segment)."/>
-			
+			<Mark desc="tlp:org ⌾ Effective Addresses tlp:br tlp:br "/>
+
+			<Mark desc="tlp:org ⌾ Physical Addresses tlp:br tlp:br A physical address is a reference into the physical-address space, typically
+			main memory. Physical addresses are translated from virtual addresses using page-translation
+			mechanisms."/>
+
 			<Head name="Operating Modes"/>
-			<div align="center"><img style={{borderRadius:"10px", width:"400px",height:"200px"}} src="https://thelinuxpoint.github.io/x86modes.png" className="put-in-side-code"/></div>
+			<div align="center"><img style={{borderRadius:"0px",filter:"invert()"}} src="https://thelinuxpoint.github.io/x86modes.png" className="put-in-side-code"/></div>
 
 			<br/>
 			<Navigator left="/cpp/intro/" right="/cpp/keywords"/>
